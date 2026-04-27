4c593e0353f739341ce79e2639d229d36121bf0a
Julian Ruiz <julianruiz@Julians-MacBook-Air.local>
2026-04-27 12:09:16 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hello
+          Learn React hello, configurando llaves ssh
         </a>
       </header>
     </div>
